4cc0e40f454e05ab20b2e5f32b017bc538439c7a
fm85 <famu1985@gmail.com>
2020-10-03 22:19:29 +0200

    BashShellTemplate.sh added

---
+++ BashShell/BashShellTemplate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#-*- coding: utf-8 -*-
+#
+#  BashShellTemplate.py
+#  
+#  Copyright (C) 2020 by GBS St. Gallen, Inc. All rights reserved.
+#  Released under the therms of the GNU General Public License version 3
+#  or later.
+#
+
+EXPECTED_NUMBER_OF_ARGUMENTS=2
+
+if [ $# -eq $EXPECTED_NUMBER_OF_ARGUMENTS ] 
+then
+    echo "argument 1 is $1 and argument 2 is $2"
+else
+    echo "$0 Error: $EXPECTED_NUMBER_OF_ARGUMENTS arguments expected!"
+fi

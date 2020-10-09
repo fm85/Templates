3bc5489cf9309ae4c148c40625e2f7cf229373d1
fm85 <famu1985@gmail.com>
2020-10-09 17:19:00 +0200

    PythonTemplateSimple.py added

---
+++ Python/PythonTemplateSimple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  PythonTemplateSimple.py
+#  
+#  Copyright (C) 2020 by GBS St. Gallen, Inc. All rights reserved.
+#  Released under the therms of the GNU General Public License version 3
+#  or later.
+#
+
+#Main function:
+def main():
+    print("Hello world")
+    return 0
+
+#Checks whether this file has been executed directly or imported from 
+#another file. Only if the file is executed directly, the main function 
+#is called.
+if __name__ == '__main__':
+    main()
